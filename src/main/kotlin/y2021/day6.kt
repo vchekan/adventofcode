d4da01a96ce57283ebbf6680212836e5cf158c01
Vadym Chekan <kot.begemot@gmail.com>
2021-12-07 19:10:29 -0800

    wip

--- src/main/kotlin/y2021/day6.kt
+++ src/main/kotlin/y2021/day6.kt
@@ -5,12 +5,12 @@ import java.io.File
 fun main() {
     val timers = //"3,4,3,1,2".split(',').map(String::toInt)
         File("data/2021/d6.txt").readLines()[0].split(',').map(String::toInt)
-    val s1 = solution1(timers, 80)
-    val s2 = solution1(timers, 256)
-    println(s1)
-    println(s2)
-    assert(s1 == 390011L)
-    assert(s2 == 1746710169834)
+    val part1 = solution2(timers, 80)
+    val part2 = solution2(timers, 256)
+    println(part1)
+    println(part2)
+    assert(part1 == 390011L)
+    assert(part2 == 1746710169834)
 }
 
 fun solution1(init: List<Int>, days: Int): Long {
@@ -25,5 +25,22 @@ fun solution1(init: List<Int>, days: Int): Long {
         counts[6] += count0
     }
 
+    return counts.sum()
+}
+
+fun solution2(init: List<Int>, days: Int): Long {
+    val counts = LongArray(9)
+    init.forEach{ timer -> counts[timer]++ }
+    var p0 = 0
+    var p6 = 6
+    for(day in 1..days) {
+        val count0 = counts[p0]
+        p0++; p6++
+        if(p0 > 8)
+            p0 = 0
+        if(p6 > 8)
+            p6 = 0
+        counts[p6] += count0
+    }
     return counts.sum()
 }
