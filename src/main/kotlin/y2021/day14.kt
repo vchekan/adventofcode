807ad34ef115800e825eac7b93fbef0c6b69917a
Vadym Chekan <kot.begemot@gmail.com>
2021-12-14 10:29:58 -0800

    d14

---
+++ src/main/kotlin/y2021/day14.kt
@@ -0,0 +1,37 @@
+package aoc.area.y2021.d14
+
+import java.io.File
+
+fun main() {
+    val lines = File("data/2021/d14.txt").readLines()
+    val template = lines.first()
+    val replacements = lines.drop(2).map {
+        val (k,v) = it.split(" -> ")
+        k to listOf("${k[0]}$v", "$v${k[1]}")
+    }.toMap()
+
+
+    val part1 = countPolymers(10, template, replacements)
+    println("Part 1: $part1")
+    assert(part1 == 3697L)
+
+    val part2 = countPolymers(40, template, replacements)
+    println(part2)
+    assert(part2 == 4371307836157L)
+
+}
+
+fun countPolymers(iterations: Int, template: String, replacements: Map<String, List<String>>): Long {
+    var templatePolymers = template.windowed(2, 1).groupingBy { it }.fold(0L) { acc, _ -> acc + 1 }
+
+    // Iterate `iterations` times, starting with the template
+    val result = (1..iterations).fold(templatePolymers) { polymers, _ ->
+        // Map every pair into 2 new pairs
+        polymers.flatMap { (polymer,count) -> replacements[polymer]!!.map { it to count } }
+            // and fold the result into (pair,count) pairs
+            .groupingBy { it.first }.fold(0L) {accumulator, element -> accumulator + element.second }
+    }
+    val counts = (result.map { (k,v) -> k[1] to v } + listOf(result.entries.first().key[0] to 1L))
+        .groupingBy { it.first }.aggregate { _, acc: Long?, element, _ -> (acc ?: 0) + element.second }
+    return counts.values.maxOrNull()!! - counts.values.minOrNull()!!
+}
