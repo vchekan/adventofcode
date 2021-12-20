d99c29755a647c5a60316b4bfd73e6a6875293ce
Vadym Chekan <kot.begemot@gmail.com>
2021-12-20 11:02:14 -0800

    wip

--- src/main/kotlin/y2021/day9.kt
+++ src/main/kotlin/y2021/day9.kt
@@ -1,69 +1,37 @@
 package y2021.d9
 
 import java.io.File
+import aoc.area.*
 
 fun main() {
-    val map = File("data/2021/d9.txt").readLines().map { it.map { it.toInt() - '0'.toInt() } }
+    val map = File("data/2021/d9.txt").readLines().map { it.map { it - '0' } }
 
-    var part1 = 0
-    for(row in map.indices) {
-        for(col in map[row].indices) {
-            if(map.isMin(row, col))
-                part1 += 1 + map[row][col]
-        }
-    }
-
+    val minBasins = map.points().filter { map.isMin(it) }
+    var part1 = minBasins.sumOf { 1 + map[it] }
     println("Part 1: $part1")
+    assert(part1 == 436)
 
-    val basins = mutableListOf<Int>()
-    for(row in map.indices) {
-        for(col in map[row].indices) {
-            if(map.isMin(row, col))
-                basins.add(map.findBasinSize(row, col))
-        }
-    }
-    basins.sortDescending()
-    val part2 = basins.take(3).reduce { acc, i -> acc * i }
+    val part2 = minBasins.map { map.findBasinSize(it) }.sortedDescending().take(3).reduce { acc, i -> acc * i }
+
     println("Part 2: $part2")
+    assert(part2 == 1317792)
 }
 
-fun List<List<Int>>.isMin(row: Int, col: Int): Boolean {
-    val self = this[row][col]
-    if(row > 0 && self >= this[row-1][col])
-        return false
-    if(row < this.size-1 && self >= this[row+1][col])
-        return false
-    if(col > 0 && self >= this[row][col-1])
-        return false
-    if(col < this[row].size-1 && self >= this[row][col+1])
-        return false
-    return true
-}
+fun List<List<Int>>.isMin(p: Point): Boolean = this.around4(p).all { this[it] > this[p] }
 
-fun List<List<Int>>.around(row: Int, col: Int): Sequence<Pair<Int,Int>> {
-    return sequence {
-        yield(Pair(row-1, col))
-        yield(Pair(row+1, col))
-        yield(Pair(row, col-1))
-        yield(Pair(row, col+1))
-    }.filter { it.first >= 0 && it.second >= 0 && it.first < this.size && it.second < this[0].size }
-}
-
-
-fun List<List<Int>>.findBasinSize(row: Int, col: Int): Int {
-    val seen = mutableSetOf<Triple<Int,Int,Int>>()
-    var newgen = listOf(Triple(row,col, this[row][col]))
+fun List<List<Int>>.findBasinSize(p: Point): Int {
+    val seen: MutableSet<Point> = mutableSetOf()
+    var newgen = listOf(p)
     while(newgen.isNotEmpty()) {
         val newgen2 = newgen
             .flatMap { oldPoint ->
-                this.around(oldPoint.first, oldPoint.second)
+                this.around4(oldPoint)
                     .filter { newpoint ->
-                        val v = this[newpoint.first][newpoint.second]
-                        v != 9 && newpoint !in seen && v > oldPoint.third
-                    }.map { Triple(it.first, it.second, this[it.first][it.second]) }
+                        val v = this[newpoint]
+                        v != 9 && newpoint !in seen && v > this[oldPoint]
+                    }
             }
-
-
+        
         seen.addAll(newgen)
         newgen = newgen2
     }
